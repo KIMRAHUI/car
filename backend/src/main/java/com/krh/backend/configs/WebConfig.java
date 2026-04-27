15910196a9c1224dab50b1948f024dd5d202e528
KIMRAHUI <rah6106670@gmail.com>
2026-04-27 17:25:33 +0900

    fix:[webconfig] 이미지 경로 수정

--- backend/src/main/java/com/krh/backend/configs/WebConfig.java
+++ backend/src/main/java/com/krh/backend/configs/WebConfig.java
@@ -1,5 +1,6 @@
 package com.krh.backend.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,18 +8,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 경로 끝에 슬래시(/)가 포함되도록 보정
+        String basePath = uploadDir.endsWith("/") ? uploadDir : uploadDir + "/";
+
         // 1. 프로필 이미지 경로
         registry.addResourceHandler("/upload/profile/**")
-                .addResourceLocations("file:///C:/Users/노오리/Desktop/carmit_uploads/profile/");
+                .addResourceLocations("file:" + basePath + "profile/");
 
         // 2. 예약 증상 이미지 경로
         registry.addResourceHandler("/upload/reservation/**")
-                .addResourceLocations("file:///C:/Users/노오리/Desktop/carmit_uploads/reservation/");
+                .addResourceLocations("file:" + basePath + "reservation/");
 
-        // 3.리뷰 이미지 경로
+        // 3. 리뷰 이미지 경로
         registry.addResourceHandler("/upload/review/**")
-                .addResourceLocations("file:///C:/Users/노오리/Desktop/carmit_uploads/review/");
+                .addResourceLocations("file:" + basePath + "review/");
     }
 }
